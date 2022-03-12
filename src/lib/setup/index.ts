1d0526dad40bec3c3fe2dcd2f88b919036c004ca
Lioness100 <jchickmm2@gmail.com>
2022-03-12 10:20:26 -0500

    Remove reflect-metadata

--- src/lib/setup/index.ts
+++ src/lib/setup/index.ts
@@ -1,7 +1,6 @@
 import { config } from 'dotenv-cra';
 process.env.NODE_ENV ??= 'development';
 
-import 'reflect-metadata';
 import '@sapphire/plugin-logger/register';
 import '@sapphire/plugin-i18next/register';
 import '@sapphire/plugin-api/register';
