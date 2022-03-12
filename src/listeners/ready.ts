00ffe2ccbfb39f211d023c8312e46dd4166d9542
lying <dev@lying.pub>
2022-03-12 09:00:51 -0800

    Fixed ready

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,12 +1,13 @@
 import { Events, Listener, ListenerOptions } from "@sapphire/framework";
 //import { captureException } from "@sentry/minimal";
 import { ApplyOptions, RequiresGuildContext } from "@sapphire/decorators";
+import { Client } from "discord.js";
 
 @ApplyOptions<ListenerOptions>({ event: Events.ClientReady })
 export class MemberAdd extends Listener {
   @RequiresGuildContext()
-  public async run() {
-      console.log(this.container.client.shard?.ids)
+  public async run(client: Client) {
+      console.log(":catyes:")
   }
     
 }
